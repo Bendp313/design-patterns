24123a4ce8ee0ba312b55b432433ff42e0027b50
Ben dela Pena <bdelapena120@gmail.com>
2021-08-30 12:34:17 -0400

    completed assignemnt

--- Strategy/LinearSearch.java
+++ Strategy/LinearSearch.java
@@ -1,8 +1,17 @@
 package Strategy;
 
 import java.util.ArrayList;
+/**
+ * A linear search 
+ * @author Ben dela Pena
+ */
 public class LinearSearch implements SearchBehavior{
-    
+    /** 
+     * Checks if the indicated person is already in the guest list using a linear search
+     * @param data The guest list of people
+     * @param item The name of the person to find
+     * @return True or false if the person is found
+     */
     public boolean contains(ArrayList<String> data, String item) {
         for (int i=0;i<data.size();i++){
             if (item.equalsIgnoreCase(data.get(i))) {
